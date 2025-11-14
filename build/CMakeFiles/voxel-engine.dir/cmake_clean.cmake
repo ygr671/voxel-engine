fcf1a58a21f50d683d7a998d36c214c1e14a4a7d
Muhammed ERDAL <m.erdal677@proton.me>
2025-11-14 20:40:36 +0100

    Project structure

---
+++ build/CMakeFiles/voxel-engine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/yzd/Documents/repos/voxel-engine/bin/voxel-engine"
+  "/home/yzd/Documents/repos/voxel-engine/bin/voxel-engine.pdb"
+  "CMakeFiles/voxel-engine.dir/src/main.cpp.o"
+  "CMakeFiles/voxel-engine.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/voxel-engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/voxel-engine.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for voxel-engine.
+# This may be replaced when dependencies are built.
